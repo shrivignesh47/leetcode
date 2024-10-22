eeec45422b496734ad6888878d989f6a42a22995
shrivignesh47 <shrivigneshshrivignesh4@gmail.com>
2024-10-22 15:21:57 +0530

    Time: 48 ms (6.92%), Space: 44.2 MB (13.01%) - LeetHub

---
+++ 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int findTheWinner(int n, int k) {
+        Queue<Integer>queue=new LinkedList<>();
+        for(int i=1;i<=n;i++)
+        {   
+         // offer == add rendum same
+            queue.add(i);
+        }
+
+        while(queue.size()>1)
+        {
+            for(int i=0;i<k-1;i++)
+            {
+                queue.add(queue.poll());
+            }
+            queue.poll();
+        }
+        return queue.peek();
+    }
+}
