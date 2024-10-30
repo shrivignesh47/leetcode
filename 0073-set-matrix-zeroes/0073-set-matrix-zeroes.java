1ff295fb9ec99678b7bf6514899953fcb2a600ab
shrivignesh47 <shrivigneshshrivignesh4@gmail.com>
2024-10-30 17:04:53 +0530

    Time: 0 ms (100%), Space: 45.6 MB (31.52%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -0,0 +1,23 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int row = matrix.length;
+        int col = matrix[0].length;
+        boolean rows[] = new boolean[row];
+        boolean cols[] = new boolean[col];
+        for(int i=0; i<row; i++) {
+            for(int j=0; j<col; j++) {
+                if(matrix[i][j] == 0) {
+                    rows[i] = true;
+                    cols[j] = true;
+                }
+            }
+        }
+        for(int i=0; i<row; i++) {
+            for(int j=0; j<col; j++) {
+                if(rows[i] || cols[j]) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+}
